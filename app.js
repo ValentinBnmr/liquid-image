1b1c5e0c5f766ad52ef0d6715e274f04e837700c
Valentin Bonnamour <valentinbonnamour@gmail.com>
2021-01-17 00:33:29 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+new hoverEffect({
+    parent: document.querySelector('.distorsion'),
+    intensity: 0.2,
+    image1: './women1.jpg',
+    image2: './women2.jpg',
+    angle: Math.PI / 4,
+    displacementImage: 'fog.png'
+
+});
